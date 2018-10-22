906ad6142127f3420a54fa963caa3378fa72f3a0
Kseniia <ksenya.smail@gmail.com>
2018-10-21 22:16:27 -0700

    background video

---
+++ assets/javascripts/index.js
@@ -0,0 +1,8 @@
+function playNext(event) {
+  const videos = Array.from(document.querySelectorAll('.content > video'));
+  const current = videos.indexOf(event.target);
+  const next = (current + 1) % videos.length;
+  videos[current].classList.toggle('hidden');
+  videos[next].classList.toggle('hidden');
+  videos[next].play();
+}
