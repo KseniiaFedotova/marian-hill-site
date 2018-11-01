87e5a7244204897af2c1ecd740be756e429f2e7f
Kseniia Fedotova <ksunich@KSENIIAs-iMac.local>
2018-10-31 23:17:21 -0700

    tours done

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,5 +1,5 @@
 function playNext(event) {
-  const videos = Array.from(document.querySelectorAll('.content > video'));
+  const videos = Array.from(document.querySelectorAll('.content video'));
   const current = videos.indexOf(event.target);
   const next = (current + 1) % videos.length;
   videos[current].classList.toggle('hidden');
--- assets/javascripts/store.js
+++ assets/javascripts/store.js
@@ -1,8 +1,4 @@
 function show(event) {
-  const selectedType = event.target.getAttribute('item-type');
-  document.querySelector('.content').classList.toggle('all', false);
-  document.querySelector('.content').classList.toggle('accessories', false);
-  document.querySelector('.content').classList.toggle('music', false);
-  document.querySelector('.content').classList.toggle('tees', false);
-  document.querySelector('.content').classList.toggle(selectedType, true);
+  document.querySelector('.content').classList.remove('all', 'accessories', 'music', 'tees');
+  document.querySelector('.content').classList.add(event.target.getAttribute('item-type'));
 }
